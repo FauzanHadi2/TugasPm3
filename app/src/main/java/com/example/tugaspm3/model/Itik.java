c1de184a6dc53e1050d6b41c52bfd4bfed562c0f
unknown <ezynino@gmail.com>
2021-12-02 18:57:00 +0800

    Initial commit

---
+++ app/src/main/java/com/example/tugaspm3/model/Itik.java
@@ -0,0 +1,10 @@
+package com.example.tugaspm3.model;
+
+public class Itik extends Hewan{
+
+    public Itik(String ras, String asal, String deskripsi, int drawableRes) {
+        super("Itik",ras,asal,deskripsi,drawableRes);
+
+    }
+
+}
